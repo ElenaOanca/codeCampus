bd0c874c4ab496eda3143203a57f38c5e5f25e2c
ElenaOanca <elenaelena.oanca@gmail.com>
2024-03-06 12:34:51 +0100

    Refactor FAQ support component and course list component

--- src/app/pages/course-list/course-list.component.ts
+++ src/app/pages/course-list/course-list.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrl: './course-list.component.scss'
 })
 export class CourseListComponent {
+  activeIndex = 0; // Indice della card attiva
+  cards = [
+    { imgSrc: '../../../assets/java.png"', title: 'Corsi JAVA', text: 'This is the first card.' },
+    { imgSrc: '../../../assets/phyton.png"', title: 'Corsi PHYTON', text: 'This is the second card.' },
+    { imgSrc: '../../../assets/angular.png"', title: 'Corsi ANGULAR', text: 'This is the third card.' },
+    { imgSrc: '../../../assets/react.png"', title: 'Corsi REACT', text: 'This is the fourth card.' },
+    { imgSrc: '../../../assets/vue.png"', title: 'Corsi VUE', text: 'This is the fifth card.' },
+    { imgSrc: '../../../assets/css.png"', title: 'Corsi JAVA', text: 'This is the first card.' },
+    { imgSrc: '../../../assets/html.png"', title: 'Corsi PHYTON', text: 'This is the second card.' },
+    { imgSrc: '../../../assets/springboot.png"', title: 'Corsi ANGULAR', text: 'This is the third card.' },
+    { imgSrc: '../../../assets/nodejs.png"', title: 'Corsi REACT', text: 'This is the fourth card.' },
+    { imgSrc: '../../../assets/js.png"', title: 'Corsi VUE', text: 'This is the fifth card.' }
 
+
+
+
+  ];
+
+  next() {
+    // Incrementa l'indice per muoversi alla prossima card
+    if (this.activeIndex < this.cards.length - 1) {
+      this.activeIndex++;
+    } else {
+      this.activeIndex = 0; // Torna all'inizio se siamo all'ultima card
+    }
+  }
+
+  previous() {
+    // Decrementa l'indice per tornare alla card precedente
+    if (this.activeIndex > 0) {
+      this.activeIndex--;
+    } else {
+      this.activeIndex = this.cards.length - 1; // Vai all'ultima card se siamo alla prima
+    }
+  }
 }
--- src/app/pages/faq-support/faq-support.component.ts
+++ src/app/pages/faq-support/faq-support.component.ts
@@ -6,5 +6,185 @@ import { Component } from '@angular/core';
   styleUrl: './faq-support.component.scss'
 })
 export class FaqSupportComponent {
+  faqs = [
+    {
+      domanda: 'Come posso iscrivermi ai corsi?',
+      risposta: 'Per iscriverti, naviga fino alla pagina del corso di interesse e clicca sul pulsante "Iscriviti ora".'
+    },
+    {
+      domanda: 'Quali metodi di pagamento sono accettati?',
+      risposta: 'Accettiamo pagamenti tramite carta di credito, PayPal, e bonifico bancario.'
+    },
+    {
+      domanda: 'Come posso iscrivermi ai corsi?',
+      risposta: 'Per iscriverti, naviga fino alla pagina del corso di interesse e clicca sul pulsante "Iscriviti ora". Dovrai fornire le tue informazioni personali e procedere al pagamento.'
+    },
+    {
+      domanda: 'Quali metodi di pagamento sono accettati?',
+      risposta: 'Accettiamo pagamenti tramite carta di credito, PayPal, e bonifico bancario. Tutti i pagamenti sono sicuri e crittografati.'
+    },
+    {
+      domanda: 'È possibile ottenere un rimborso se il corso non soddisfa le mie aspettative?',
+      risposta: 'Sì, offriamo una garanzia di rimborso di 30 giorni per tutti i nostri corsi. Se non sei soddisfatto del corso, puoi richiedere un rimborso entro 30 giorni dall\'acquisto.'
+    },
+
+    {
+      domanda: 'I corsi sono accessibili a vita?',
+      risposta: 'Sì, una volta acquistato un corso, avrai accesso a vita al materiale didattico, compresi tutti gli aggiornamenti futuri.'
+    },
+    {
+      domanda: 'Posso scaricare il materiale del corso?',
+      risposta: 'Dipende dal corso specifico. Alcuni corsi possono offrire materiali scaricabili, mentre altri potrebbero limitare l\'accesso al solo streaming online per proteggere i diritti d\'autore.'
+    },
+    {
+      domanda: 'Cosa succede se ho bisogno di aiuto durante il corso?',
+      risposta: 'Offriamo supporto tecnico e didattico tramite email, chat, e forum dedicati agli studenti. Non esitare a contattarci se hai bisogno di aiuto.'
+    },
+    {
+      domanda: 'Ci sono prerequisiti per iscriversi ai corsi?',
+      risposta: 'Molti corsi sono progettati per principianti e non richiedono conoscenze pregresse. Tuttavia, alcuni corsi avanzati possono avere prerequisiti specifici, che sono chiaramente elencati nella pagina del corso.'
+    },
+    {
+      domanda: 'Posso ottenere un certificato al termine del corso?',
+      risposta: 'Sì, al completamento di un corso, verrà rilasciato un certificato di completamento che potrai stampare o condividere sui social media e LinkedIn.'
+    },
+    {
+      domanda: 'I corsi sono aggiornati con le ultime tecnologie?',
+      risposta: 'Assolutamente. Aggiorniamo regolarmente i nostri corsi per riflettere le ultime tendenze e tecnologie del settore informatico.'
+    },
+    {
+      "domanda": "Come posso iniziare con Java?",
+      "risposta": "Per iniziare con Java, ti consigliamo di esplorare la documentazione ufficiale e seguire alcuni tutorial introduttivi online."
+    },
+    {
+      "domanda": "Quali sono le migliori risorse per imparare Java?",
+      "risposta": "Le migliori risorse per imparare Java includono la documentazione ufficiale, corsi online su piattaforme come Udemy o Coursera, e tutorial su YouTube."
+    },
+    {
+      "domanda": "Quali progetti posso realizzare per migliorare le mie competenze in Java?",
+      "risposta": "Per migliorare le tue competenze in Java, considera di lavorare su progetti personali, contribuire a progetti open source o risolvere problemi su siti come HackerRank e LeetCode."
+    },
+    {
+      "domanda": "Come posso restare aggiornato sulle ultime novità in Java?",
+      "risposta": "Per restare aggiornato su Java, segui blog di settore, iscriviti a newsletter specializzate e partecipa a conferenze e meetup."
+    },
+    {
+      "domanda": "Ci sono certificazioni che posso ottenere per dimostrare le mie competenze in Java?",
+      "risposta": "Per Java, esistono diverse certificazioni riconosciute nel settore che puoi ottenere dopo aver superato gli esami pertinenti. Esplora quelle offerte da enti certificatori come Oracle per Java o Microsoft per alcune tecnologie."
+    },
+    {
+      "domanda": "Come posso iniziare con Python?",
+      "risposta": "Per iniziare con Python, ti consigliamo di esplorare la documentazione ufficiale e seguire alcuni tutorial introduttivi online."
+    },
+    {
+      "domanda": "Quali sono le migliori risorse per imparare Python?",
+      "risposta": "Le migliori risorse per imparare Python includono la documentazione ufficiale, corsi online su piattaforme come Udemy o Coursera, e tutorial su YouTube."
+    },
+    {
+      "domanda": "Quali progetti posso realizzare per migliorare le mie competenze in Python?",
+      "risposta": "Per migliorare le tue competenze in Python, considera di lavorare su progetti personali, contribuire a progetti open source o risolvere problemi su siti come HackerRank e LeetCode."
+    },
+    {
+      "domanda": "Come posso restare aggiornato sulle ultime novità in Python?",
+      "risposta": "Per restare aggiornato su Python, segui blog di settore, iscriviti a newsletter specializzate e partecipa a conferenze e meetup."
+    },
+    {
+      "domanda": "Ci sono certificazioni che posso ottenere per dimostrare le mie competenze in Python?",
+      "risposta": "Per Python, esistono diverse certificazioni riconosciute nel settore che puoi ottenere dopo aver superato gli esami pertinenti. Esplora quelle offerte da enti certificatori come Python Institute."
+    },
+    {
+      "domanda": "Come posso iniziare con JavaScript?",
+      "risposta": "Per iniziare con JavaScript, ti consigliamo di esplorare la documentazione ufficiale e seguire alcuni tutorial introduttivi online."
+    },
+    {
+      "domanda": "Quali sono le migliori risorse per imparare JavaScript?",
+      "risposta": "Le migliori risorse per imparare JavaScript includono la documentazione ufficiale, corsi online su piattaforme come Udemy o Coursera, e tutorial su YouTube."
+    },
+    {
+      "domanda": "Quali progetti posso realizzare per migliorare le mie competenze in JavaScript?",
+      "risposta": "Per migliorare le tue competenze in JavaScript, considera di lavorare su progetti personali, contribuire a progetti open source o risolvere problemi su siti come HackerRank e LeetCode."
+    },
+     {
+      "domanda": "Come posso restare aggiornato sulle ultime novità in JavaScript?",
+      "risposta": "Per restare aggiornato su JavaScript, segui blog di settore, iscriviti a newsletter specializzate e partecipa a conferenze e meetup."
+    },
+    {
+      "domanda": "Ci sono certificazioni che posso ottenere per dimostrare le mie competenze in JavaScript?",
+      "risposta": "Per JavaScript, esistono diverse certificazioni riconosciute nel settore che puoi ottenere dopo aver superato gli esami pertinenti. Esplora quelle offerte da enti certificatori come Microsoft per sviluppatori."
+    }, {
+      "domanda": "Come posso iniziare con Angular?",
+      "risposta": "Per iniziare con Angular, ti consigliamo di esplorare la documentazione ufficiale e seguire alcuni tutorial introduttivi online."
+    },
+    {
+      "domanda": "Quali sono le migliori risorse per imparare Angular?",
+      "risposta": "Le migliori risorse per imparare Angular includono la documentazione ufficiale, corsi online su piattaforme come Udemy o Coursera, e tutorial su YouTube."
+    },
+    {
+      "domanda": "Quali progetti posso realizzare per migliorare le mie competenze in Angular?",
+      "risposta": "Per migliorare le tue competenze in Angular, considera di lavorare su progetti personali, contribuire a progetti open source o risolvere problemi su siti come HackerRank e LeetCode."
+    },
+    {
+      "domanda": "Come posso restare aggiornato sulle ultime novità in Angular?",
+      "risposta": "Per restare aggiornato su Angular, segui blog di settore, iscriviti a newsletter specializzate e partecipa a conferenze e meetup."
+    },
+    {
+      "domanda": "Ci sono certificazioni che posso ottenere per dimostrare le mie competenze in Angular?",
+      "risposta": "Per Angular, non esistono certificazioni ufficiali rilasciate da Google, ma puoi trovare corsi certificati da terze parti che offrono credenziali riconosciute nel settore."
+    },
+    {
+      "domanda": "Come posso iniziare con Spring Boot?",
+      "risposta": "Per iniziare con Spring Boot, ti consigliamo di esplorare la documentazione ufficiale e seguire alcuni tutorial introduttivi online."
+    },
+    {
+      "domanda": "Quali sono le migliori risorse per imparare Spring Boot?",
+      "risposta": "Le migliori risorse per imparare Spring Boot includono la documentazione ufficiale, corsi online su piattaforme come Udemy o Coursera, e tutorial su YouTube."
+    },
+    {
+      "domanda": "Quali progetti posso realizzare per migliorare le mie competenze in Spring Boot?",
+      "risposta": "Per migliorare le tue competenze in Spring Boot, considera di lavorare su progetti personali, contribuire a progetti open source o risolvere problemi su siti come HackerRank e LeetCode."
+    },
+    {
+      "domanda": "Come posso restare aggiornato sulle ultime novità in Spring Boot?",
+      "risposta": "Per restare aggiornato su Spring Boot, segui blog di settore, iscriviti a newsletter specializzate e partecipa a conferenze e meetup."
+    },
+    {
+      "domanda": "Ci sono certificazioni che posso ottenere per dimostrare le mie competenze in Spring Boot?",
+      "risposta": "Sì, Spring offre la certificazione Spring Professional, che puoi ottenere dopo aver superato un esame che testa la tua conoscenza di Spring Framework e Spring Boot."
+    },
+    {
+      "domanda": "Come posso iniziare con Bootstrap?",
+      "risposta": "Per iniziare con Bootstrap, visita il sito ufficiale e segui la documentazione per imparare come incorporare il framework nel tuo progetto web."
+    },
+    {
+      "domanda": "Quali sono le migliori risorse per imparare HTML e CSS?",
+      "risposta": "Per HTML e CSS, siti come W3Schools, MDN Web Docs e tutorial su YouTube offrono ottime guide e lezioni per principianti ed esperti."
+    },
+    {
+      "domanda": "Come posso iniziare con C++?",
+      "risposta": "Per iniziare con C++, considera di leggere libri raccomandati come 'Programming: Principles and Practice Using C++' di Bjarne Stroustrup e praticare tramite esercizi online."
+    },
+    {
+      "domanda": "Come posso migliorare le mie abilità di problem solving in programmazione?",
+      "risposta": "Per migliorare le tue abilità di problem solving, pratica regolarmente su siti come LeetCode, HackerRank e CodeSignal. Affronta una varietà di problemi e cerca di capire approcci e soluzioni diverse."
+      },
+      {
+      "domanda": "Esistono community o forum dove posso chiedere aiuto su dubbi di programmazione?",
+      "risposta": "Sì, esistono numerose community online dove puoi chiedere aiuto, tra cui Stack Overflow, Reddit (subreddit specifici come r/learnprogramming), e forum dedicati su siti di corsi come Coursera e Udemy."
+      },
+      {
+      "domanda": "Qual è il modo migliore per rimanere aggiornati sulle nuove tecnologie e linguaggi di programmazione?",
+      "risposta": "Per rimanere aggiornati, segui blog di tecnologia, iscriviti a newsletter specifiche, partecipa a conferenze e webinar, e unisciti a gruppi o community professionali su piattaforme come LinkedIn."
+      },
+      {
+      "domanda": "Come posso scegliere quale linguaggio di programmazione imparare per primo?",
+      "risposta": "Scegli basandoti sugli ambiti di tuo interesse (web development, app mobile, data science, ecc.) e sulla domanda di mercato. Linguaggi come Python, JavaScript e Java sono spesso raccomandati per i principianti."
+      },
+      {
+      "domanda": "Quali sono i migliori editor di codice o IDE per la programmazione?",
+      "risposta": "Le scelte popolari includono Visual Studio Code, JetBrains IntelliJ IDEA, PyCharm per Python, e Eclipse per Java. La scelta dipende dal linguaggio di programmazione e dalle preferenze personali."
+      }
+
+
+  ];
 
 }
