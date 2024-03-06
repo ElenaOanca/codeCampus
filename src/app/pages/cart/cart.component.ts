44b58d859947329ffc9525b58cf961fa0be69096
ElenaOanca <elenaelena.oanca@gmail.com>
2024-03-06 18:57:52 +0100

    Add Bootstrap Icons and AOS library to styles in angular.json

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+interface CartItem {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+  cartItems: CartItem[] = [];
 
+  addItem(index: number) {
+    this.cartItems[index].quantity++;
+  }
+
+  removeItem(index: number) {
+    if (this.cartItems[index].quantity > 1) {
+      this.cartItems[index].quantity--;
+    } else {
+      this.cartItems.splice(index, 1);
+    }
+  }
+
+  getTotal(): number {
+    return this.cartItems.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+  }
+
+  checkout() {
+    // Simulare il salvataggio degli articoli del carrello e il redirect alla pagina di conferma
+    console.log("Articoli del carrello:", this.cartItems);
+    console.log("Totale:", this.getTotal());
+    // Qui potresti navigare alla tua pagina di conferma con i dati del carrello
+  }
 }
