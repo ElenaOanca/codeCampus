2fe4365ceb8552185a5fab68895444494239083e
ElenaOanca <elenaelena.oanca@gmail.com>
2024-03-06 19:45:55 +0100

    Update footer and blog-news components

--- src/app/pages/blog-news/blog-news.component.ts
+++ src/app/pages/blog-news/blog-news.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blog-news',
   templateUrl: './blog-news.component.html',
-  styleUrl: './blog-news.component.scss'
+  styleUrls: ['./blog-news.component.scss']
 })
-export class BlogNewsComponent {
+export class BlogNewsComponent implements OnInit {
+
+  blogPosts: { id: number, title: string, description: string }[] = [
+    { id: 1, title: 'Titolo articolo 1', description: 'Descrizione articolo 1' },
+    { id: 2, title: 'Titolo articolo 2', description: 'Descrizione articolo 2' },
+    // Aggiungi altri articoli del blog
+  ];
+
+  newsItems: { id: number, title: string, description: string }[] = [
+    { id: 1, title: 'Titolo notizia 1', description: 'Descrizione notizia 1' },
+    { id: 2, title: 'Titolo notizia 2', description: 'Descrizione notizia 2' },
+    // Aggiungi altre notizie
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
